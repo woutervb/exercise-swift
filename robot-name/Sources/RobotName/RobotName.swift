384727fdffa113bad3d4eb5f151f66cbea67c496
Wouter van Bommel <wouter@woutervb.net>
2024-09-11 13:38:46 +1000

    added solution

--- robot-name/Sources/RobotName/RobotName.swift
+++ robot-name/Sources/RobotName/RobotName.swift
@@ -1,1 +1,16 @@
 //Solution goes in Sources
+struct Robot {
+    var name: String
+    
+    init() {
+        let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let numbers = "0123456789"
+        let randomLetters = String((0..<2).map { _ in letters.randomElement()! })
+        let randomNumbers = String( (0..<3).map { _ in numbers.randomElement()! })
+        name = "\(randomLetters)\(randomNumbers)"
+    }
+
+     mutating func resetName() {
+        self.name = Robot().name
+    }
+}
