ad4f3526c7637045f6be8debdc270fcd09ba3b7e
Wouter van Bommel <wouter@woutervb.net>
2024-09-11 16:27:16 +1000

    added solution

--- strain/Sources/Strain/Strain.swift
+++ strain/Sources/Strain/Strain.swift
@@ -1,22 +1,10 @@
 extension Array {
   func keep(_ predicate: (Element) -> Bool) -> [Element] {
-    var result: [Element] = []
-    for element in self {
-      if predicate(element) {
-        result.append(element)
-      }
-    }
-    return result
+    return self.filter(predicate)
   }
 
 func discard(_ predicate: (Element) -> Bool) -> [Element] {
-    var result: [Element] = []
-    for element in self {
-      if !predicate(element) {
-        result.append(element)
-      }
-    }
-    return result
+    return self.filter { !predicate($0) }
   }
 }
 
